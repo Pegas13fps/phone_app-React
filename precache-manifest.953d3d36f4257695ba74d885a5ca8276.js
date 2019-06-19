f19d90bdfcf67e0ce2d6054bc5db239a501d59c3
Pegas13fps <Tymoschenkob@gmail.com>
2019-06-19 20:43:24 +0300

    Updates

---
+++ precache-manifest.953d3d36f4257695ba74d885a5ca8276.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25f857d4d3878488d93289c8251a56fb",
+    "url": "/phone_app-React/index.html"
+  },
+  {
+    "revision": "ed61bfd5477508fcabec",
+    "url": "/phone_app-React/static/css/main.4ff0b4a7.chunk.css"
+  },
+  {
+    "revision": "35c327d28054c4fa532d",
+    "url": "/phone_app-React/static/js/2.fcec4472.chunk.js"
+  },
+  {
+    "revision": "ed61bfd5477508fcabec",
+    "url": "/phone_app-React/static/js/main.577ccda6.chunk.js"
+  },
+  {
+    "revision": "5f90298abde68a7f9391",
+    "url": "/phone_app-React/static/js/runtime~main.08c6525c.js"
+  }
+]);
